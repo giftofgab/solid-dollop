1604325f37577d3facf5f3a5ce34f05e3758d0f7
Gaby <Gaby@elcapitans-MacBook-Pro.local>
2023-02-22 21:08:35 -0800

    Putting the code back to where it was

--- MyScrapingCode.py
+++ MyScrapingCode.py
@@ -4,31 +4,28 @@ from requests_html import HTMLSession
 import requests
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-url = 'https://www.etsy.com/search?q=digital+planner&ref=pagination&page=1&order=price_asc'
+url = 'https://www.etsy.com/search?q=digital+planner&ref=pagination&page=1'
 
 response=requests.get(url,headers=headers)
-soup=BeautifulSoup(response.content, 'lxml')
+soup=BeautifulSoup(response.content,'html.parser')
 
-items=soup.find_all("a", {"class": "listing-link"})
-for item in soup.find_all("a", {"class": "listing-link"}):
-# for item in soup.select('.listing-link'):
+for item in soup.select('.listing-link'):
     try:
         print('----------------------------------------')
-        print(item.text.replace("\n", "")[:100])
         ## Title
-        # print(item.select('h3')[0].get_text().strip())
+        print(item.select('h3')[0].get_text().strip())
         ## Ads
-        # print(item.select('p')[4].get_text().strip())
+        print(item.select('p')[4].get_text().strip())
         ## Price
-        # print(item.select('p')[2].get_text().strip())
+        print(item.select('p')[2].get_text().strip())
         ## Store
-        # print(item.select('p')[5].get_text().strip())
+        print(item.select('p')[5].get_text().strip())
         ## Stars
-        # print(item.select('span')[2].get_text().strip())
+        print(item.select('span')[2].get_text().strip())
         ## Reviews
-        # print(item.select('span')[4].get_text().strip())
+        print(item.select('span')[4].get_text().strip())
         ## URL
-        # print(item.select('.width-full')[0]['src'])
+        print(item.select('.width-full')[0]['src'])
         ##print(item.select('.currency-value')[0].get_text().strip())
         ##print(item.select('.screen-reader-only')[0].get_text().strip())
         ##print(item.select('.width-full')[0]['src'])
