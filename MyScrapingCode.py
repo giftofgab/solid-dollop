77e8c5b02ec313bea8816f7593f3b0819163c248
Gaby <roestandy9@gmail.com>
2023-02-25 16:36:17 -0800

    ad update

--- MyScrapingCode.py
+++ MyScrapingCode.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 
-df = pd.DataFrame(columns=['Title', 'Price','Reviews', 'Store','URL','Page Number','Page Rank','Seller Status'])
+df = pd.DataFrame(columns=['Title', 'Original Price', 'Sale Price', 'Reviews', 'Store', 'URL', 'Page Number', 'Page Location', 'Seller Status', 'Ad Status'])
 
 for page_num in range(1, 6):
     url = f'https://www.etsy.com/search?explicit=1&q=planner&order=most_relevant&page={page_num}&ref=pagination'
@@ -20,8 +20,13 @@ for page_num in range(1, 6):
             ## Title
             row['Title'] = item.find('h3', {'class': 'wt-text-caption v2-listing-card__title wt-text-truncate'}).text.strip()
             ## Price
-            for price in item.find_all('span', {"class": "currency-value"}):
-                row['Price'] = price.get_text().strip()
+            prices = item.find_all('span', {"class": "currency-value"})
+            if len(prices) == 1:
+                row['Sale Price'] = prices[0].get_text().strip()
+                row['Original Price'] = None
+            elif len(prices) == 2:
+                row['Sale Price'] = prices[0].get_text().strip()
+                row['Original Price'] = prices[1].get_text().strip()
             ## Reviews
             row['Reviews'] = item.find('span', {'class': 'wt-text-caption wt-text-gray wt-display-inline-block wt-nudge-l-3 wt-pr-xs-1'}).text.strip()
             ## Store
@@ -43,13 +48,13 @@ for page_num in range(1, 6):
                 row['Seller Status'] = 'Star Seller'
             else:
                 row['Seller Status'] = 'Not a Star Seller'
-            ## Ad
-            # ad_badge = item.find('div', {'class': 'wt-text-caption wt-text-truncate wt-text-grey wt-mb-xs-1 min-height'})
-            # is_ad = ad_badge is not None
-            # if is_ad:
-            #     print('Ad')
-            # else:
-            #     print('Not an Ad')
+            # Ad
+            ad_badge = item.find('div', {'class': 'wt-text-caption wt-text-truncate wt-text-grey wt-mb-xs-1 min-height'})
+            is_ad = ad_badge.find('p')
+            if is_ad is not None and is_ad.text == 'Ad vertisement by Etsy seller':
+                row['Ad Status'] = 'Ad'
+            else:
+                row['Ad Status'] = 'Not an Ad'
 
         except Exception as e:
             # Handle any errors that may occur during extraction
